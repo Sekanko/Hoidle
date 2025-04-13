fd149d38e568baf9a1b1b2c130b89c7f70c03819
Oskar Tarka <oskar0208@gmail.com>
2025-04-13 13:26:52 +0200

    initial commit with hoidle-data module template

---
+++ hoidle-data/src/main/java/pl/sekankodev/hoidledata/HoidleDataApplication.java
@@ -0,0 +1,13 @@
+package pl.sekankodev.hoidledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoidleDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoidleDataApplication.class, args);
+    }
+
+}
