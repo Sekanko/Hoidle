337d35ac39de78f2bdebd80694dff244d35f4ab6
Oskar Tarka <oskar0208@gmail.com>
2025-05-05 14:36:36 +0200

    Wrong annotation fixed

--- src/main/java/pl/sekankodev/hoidle/HoidleApplication.java
+++ src/main/java/pl/sekankodev/hoidle/HoidleApplication.java
@@ -3,11 +3,12 @@ package pl.sekankodev.hoidle;
 import jakarta.persistence.Cacheable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "pl.sekankodev")
-@Cacheable
+@EnableCaching
 public class HoidleApplication {
     public static void main(String[] args) {
         SpringApplication.run(HoidleApplication.class, args);
